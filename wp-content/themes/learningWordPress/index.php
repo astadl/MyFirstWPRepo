9c47af9c06ebac543a25f994f97de135cf1c66b0
astadl <45481373+astadl@users.noreply.github.com>
2019-01-17 21:01:06 +0100

    Add files via upload

--- wp-content/themes/learningWordPress/index.php
+++ wp-content/themes/learningWordPress/index.php
@@ -5,8 +5,8 @@ get_header();
 if (have_posts()) :
 	while (have_posts()): the_post(); ?>
 	
-	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h2>
-	<?php the_content(); ?>
+	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	<?php the_content(null, false); ?> 
 <?php	endwhile;
 	
 	else:
